8990680f2437ffb71f414c0969dac69918f30b27
苏苏苏susie <1073390003@qq.com>
2019-07-03 23:10:38 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function  toFlag() {
+    window.location.href="flag.html";
+}
+
+function  toForm() {
+    console.log("form")
+    var msg='<div ><form><label>姓名：</label><input type="text" class=""></form></div>';
+    bootbox.dialog({
+        title: "",
+        message: msg,
+        buttons: {
+            cancel: {
+                label: '取消',
+            },
+            success: {
+                label: "确认提交",
+                className: "btn-primary",
+                callback: function () {
+
+
+                }
+            },
+        }
+    });
+}
+
+function downPicture(){
+    console.log("picture")
+}
