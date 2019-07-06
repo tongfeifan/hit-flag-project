e86dbce4fe4d9bb6d6d92e63e1ee0b496b98b512
苏苏苏susie <1073390003@qq.com>
2019-07-06 19:24:16 +0800

    change the pictures randomly in flag.html

--- js/main.js
+++ js/main.js
@@ -25,6 +25,31 @@ function  toForm() {
     });
 }
 
+function getRandom(){
+    var text_arr=["你好","我好","大家好","上班","太累","回家睡"];
+    var n=Math.floor(Math.random()*6);
+    console.log(text_arr[n]);
+    var img=n+".jpg";
+    var str="<img src='img/"+img+"' class='img-flag' width='97%' height='100%' >";
+    $(".main-content").append(str);
+}
+
 function downPicture(){
     console.log("picture")
+    var url='http://47.101.11.38/img/0.jpg'
+    // var download=plus.downloader.createDownload(url, {}, function(d,status){
+    //     // 下载完成
+    //     if(status == 200){
+    //         alert("Download success: " + d.filename);
+    //     } else {
+    //         alert("Download failed: " + status);
+    //     }
+    // });
+    // download.start();
+
+    // var str="<a href="+url+" download='HITflag'></a>"
+    // $("#down").append(str)
+
 }
+
+
