dd4935310ddc6d0c2847804e67cb407e0b3915a5
zcy93916 <zcy93916@163.com>
2019-07-14 23:31:29 +0800

    change picture loading mode

--- js/main.js
+++ js/main.js
@@ -72,14 +72,27 @@ function downPicture(){
 
 $(function(){
     
-    var imgUrl = 'img/long_pic.jpg';
+    var imgUrl = 'img/long_pic1.jpg';
     var imgObject = new Image();
     imgObject.src = imgUrl;
 
     imgObject.onload = function(){
-        var ele = document.querySelector('.thumbnails');
-        ele.setAttribute('src', imgUrl);
-        ele.setAttribute('class', 'thumbnails complete');
+        var pic1 = document.querySelector('#long_pic1');
+        pic1.setAttribute('src', imgUrl);
+        pic1.setAttribute('class', 'thumbnails complete');
+
+        var pic2 = document.querySelector('#long_pic2');
+        pic2.setAttribute('src', 'img/long_pic2.jpg');
+
+        var pic3 = document.querySelector('#long_pic3');
+        pic3.setAttribute('src', 'img/long_pic3.jpg');
+
+        var pic4 = document.querySelector('#long_pic4');
+        pic4.setAttribute('src', 'img/long_pic4.jpg');
+
+        var pic5 = document.querySelector('#long_pic5');
+        pic5.setAttribute('src', 'img/long_pic5.jpg');
+
     };
  
 });
