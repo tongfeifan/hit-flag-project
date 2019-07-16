60bb17142f42340acf3f2d10ac56c81f447d21c0
Feifan Tong <tongfeifan@foxmail.com>
2019-07-16 16:02:15 +0800

    1

--- js/main.js
+++ js/main.js
@@ -62,15 +62,21 @@ function getRank(){
 }
 function getRandom(rank){
     var text_arr=[
-        "学霸手贱，发射了一枚火箭把太阳弄灭了。”安息吧,人类”!!!校旗传递0KM。",
-        "高考填报了软件工程专业，大学四年没有找到女朋友，校旗传递0KM",
-        
-        "你好","我好","大家好","上班","太累","回家睡"
+        "学霸手贱，发射了一枚火箭把太阳弄灭了。”安息吧,人类”!!!校旗传递了0KM。",
+        "高考填报了软件工程专业，大学四年没有找到女朋友，校旗传递了0KM",
+        "找到了志愿者筹备组，行贿了贝勒爷。校旗传递了99999KM。",
+        "参加了上海校友会户外协会的活动，把校旗插在了珠穆朗玛峰上，校旗传递8844.43米。",
+        "从软件工程毕业后，进入了DJI，用无人机把校旗带上了天，校旗传递20KM。",
+        "跟随吴献斌，参与了横跨台湾海峡活动，校旗传递400kM。",
+        "报名参加了辽宁号的护航行动，校旗传递10000KM。"
     ];
-    var n=Math.floor(Math.random()*4);
+    var text_n=Math.floor(Math.random()*7);
+    var n=Math.floor(Math.random()*5);
     console.log(text_arr[n]);
     var imgNo=n+".jpg";
     let img = document.getElementById("random-img");
+    
+    var text = '童飞帆' + text_arr[text_n]
 
     let mc = new window.MCanvas.default({
         width: 1000,
@@ -85,9 +91,9 @@ function getRandom(rank){
         color:'#000000',
         type:'origin',
     })
-        .text('郭晓东，' + text_arr[n] + ', 成为第'+ rank +'位校旗传递者',{
+        .text(text + '成为第'+ rank +'位校旗传递者',{
             width:'300px',
-            align:'center',
+            align:'left',
             pos:{
                 x:250,
                 y:50,
@@ -99,7 +105,7 @@ function getRandom(rank){
 
             console.log(b64);
         });
-    shareSet(rank, text_arr)
+    shareSet(rank, text)
 
 }
 
